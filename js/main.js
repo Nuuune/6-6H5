0790cc1f9ad8cb9f68cd8eea960d93fbd3cb90f6
Nuuune <971061461@qq.com>
2018-06-09 22:27:41 +0800

    添加预载资源

--- js/main.js
+++ js/main.js
@@ -1,20 +1,93 @@
 (function(){
 
-  let icons = [
-    './imgs/足球.png',
-    './imgs/网球.png',
-    './imgs/篮球.png',
-    './imgs/电竞.png'
+  let allImags = [
+    '/imgs/01b.png',
+    '/imgs/02b.png',
+    '/imgs/03b.png',
+    '/imgs/04b.png',
+    '/imgs/bg_main.jpg',
+    '/imgs/btn_agin.png',
+    '/imgs/btn_more.png'
   ];
 
-  let app = document.getElementById("app");
-  let mainPage = document.getElementById("mpage");
-  let video = document.getElementById("myvideo");
-  let playBtn = document.getElementById("playBtn");
-  let picTimer = new PicTimer(icons, 5);
-  // picTimer.run(app, ()=> alert(`完成`));
-  playBtn.addEventListener("click", function(){
-    video.play();
-  },false)
+  // 预处理多张图片
+  function PreLoadImg() {
+      this.imgs = [];
+      this.total = 0;
+      this.finished = 0;
+      this.onAllLoaded = () => console.log(`请设置onAllLoaded回调`);
+  }
+  PreLoadImg.prototype = {
+      isAllLoaded: function() {
+          return this.total === this.finished;
+      },
+      addSrc: function(imgsUrl) {
+          let img;
+          this.total = imgsUrl.length;
+          imgsUrl.forEach(item => {
+              img = new Image();
+              img.src = item;
+              img.onload = () => {
+                this.finished++;
+                if(this.isAllLoaded()){
+                  this.emitAllLoaded();
+                }
+              };
+              this.imgs.push(img);
+          })
+      },
+      getImgs: function() {
+          return this.imgs;
+      },
+      emitAllLoaded: function() {
+          this.onAllLoaded(this.imgs);
+      }
+  }
 
+
+
+  let preImg = new PreLoadImg();
+  preImg.addSrc(allImags);
+  preImg.onAllLoaded = function() {
+    run();
+  }
+
+
+  function run() {
+    let app = document.getElementById("app");
+    let mainPage = document.getElementById("mpage");
+    let vpage= document.getElementById("vpage"); // vdieo wrap
+    let video = document.getElementById("myvideo"); // video element
+    let vbtn = document.getElementById("vbtn"); // video Btns wrap
+    let playBtn = document.getElementById("playBtn"); // video play btn
+    let picTimer = new PicTimer(allImags.slice(0,4), 5);
+
+    picTimer.run(app, ()=> {
+      show(vpage);
+      show(vbtn);
+      hidden(app);
+    });
+
+    playBtn.addEventListener("click", function(){
+      video.play();
+      hidden(vbtn);
+    },false)
+
+    // 播放结束 隐藏video
+    video.addEventListener("ended", function(){
+      hidden(vpage);
+      hidden(video);
+      show(mainPage);
+    }, false)
+  }
+
+
+  function hidden(e) {
+    e.classList.remove(`show`);
+    e.classList.add(`hidden`);
+  }
+  function show(e) {
+    e.classList.remove(`hidden`);
+    e.classList.add(`show`);
+  }
 }).call(this);
