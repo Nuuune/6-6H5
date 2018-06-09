d83011775c77a7724319a0faab6c094844baecb4
nuuune <971061461@qq.com>
2018-06-09 18:43:09 +0800

    需要完成video

--- js/main.js
+++ js/main.js
@@ -8,7 +8,13 @@
   ];
 
   let app = document.getElementById("app");
+  let mainPage = document.getElementById("mpage");
+  let video = document.getElementById("myvideo");
+  let playBtn = document.getElementById("playBtn");
   let picTimer = new PicTimer(icons, 5);
-  picTimer.run(app, ()=> alert(`完成`));
+  // picTimer.run(app, ()=> alert(`完成`));
+  playBtn.addEventListener("click", function(){
+    video.play();
+  },false)
 
 }).call(this);
